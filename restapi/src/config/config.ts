eeb67614c6f83972ec3ef752122c7879541bdc43
Madejczyk <jakub.madejczyk92@gmail.com>
2020-03-31 11:14:26 +0200

    fix tslint for restapi

--- restapi/src/config/config.ts
+++ restapi/src/config/config.ts
@@ -1,19 +1,17 @@
 export const config = {
-  "dev": {
+  "postgress": {
     "username": process.env.PROGRESS_USERNAME,
     "password": process.env.PROGRESS_PASSWORD,
     "database": process.env.PROGRESS_DATABASE,
     "host": process.env.PROGRESS_HOST,
     "dialect": "postgres",
+  },
+  "aws": {
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
-  "prod": {
-    "username": process.env.PROGRESS_USERNAME,
-    "password": process.env.PROGRESS_PASSWORD,
-    "database": process.env.PROGRESS_DATABASE,
-    "host": process.env.PROGRESS_HOST,
-    "dialect": "postgres"
+  "jwt": {
+    "secret": process.env.JWT_SECRET
   }
 }
