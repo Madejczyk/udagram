74047ebe318550d89714a89b36e26bd584572566
Madejczyk <jakub.madejczyk92@gmail.com>
2020-03-31 15:07:33 +0200

    fix auth return token

--- restapi/src/config/config.ts
+++ restapi/src/config/config.ts
@@ -1,17 +1,17 @@
 export const config = {
-  "postgress": {
-    "username": process.env.PROGRESS_USERNAME,
-    "password": process.env.PROGRESS_PASSWORD,
-    "database": process.env.PROGRESS_DATABASE,
-    "host": process.env.PROGRESS_HOST,
-    "dialect": "postgres",
+  'postgress': {
+    'username': process.env.PROGRESS_USERNAME,
+    'password': process.env.PROGRESS_PASSWORD,
+    'database': process.env.PROGRESS_DATABASE,
+    'host': process.env.PROGRESS_HOST,
+    'dialect': 'postgres',
   },
-  "aws": {
-    "aws_region": process.env.AWS_REGION,
-    "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+  'aws': {
+    'aws_region': process.env.AWS_REGION,
+    'aws_profile': process.env.AWS_PROFILE,
+    'aws_media_bucket': process.env.AWS_MEDIA_BUCKET
   },
-  "jwt": {
-    "secret": process.env.JWT_SECRET
+  'jwt': {
+    'secret': process.env.JWT_SECRET
   }
-}
+};
